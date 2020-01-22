89cece55a8ca1d906f6e14a834e3354804111333
EboThecoder <thegreatebo@gmail.com>
2020-01-22 20:44:12 -0300

    adt da hash.h feita

--- huffman/code/heap.h
+++ huffman/code/heap.h
@@ -6,33 +6,93 @@
 #include <stdlib.h>
 #include <limits.h>
 
-
+/**
+ * @brief
+ * 
+ */
 typedef struct heap
 {
     int size; 
     node *nodes[256];
 } heap;
-
+/**
+ * @brief Create a heap object
+ * 
+ * @return heap* 
+ */
 heap *create_heap();
-
+/**
+ * @brief Get the parent index object
+ * 
+ * @param heap 
+ * @param i 
+ * @return int 
+ */
 int get_parent_index(heap *heap, int i);
-
+/**
+ * @brief Get the left index object
+ * 
+ * @param heap 
+ * @param i 
+ * @return int 
+ */
 int get_left_index(heap *heap, int i);
-
+/**
+ * @brief Get the right index object
+ * 
+ * @param heap 
+ * @param i 
+ * @return int 
+ */
 int get_right_index(heap *heap, int i);
-
+/**
+ * @brief 
+ * 
+ * @param u 
+ * @param v 
+ * @param len 
+ */
 void swap(void *u, void *v, size_t len);
-
+/**
+ * @brief 
+ * 
+ * @param heap 
+ */
 void print_heap(heap *heap);
- 
+/**
+ * @brief 
+ * 
+ * @param heap 
+ * @param i 
+ */
 void min_heapify(heap *heap, int i);
-
+/**
+ * @brief 
+ * 
+ * @param heap 
+ * @param item 
+ * @return int 
+ */
 int is_in_heap(heap *heap, void *item);
-
+/**
+ * @brief 
+ * 
+ * @param heap 
+ * @param new_node 
+ */
 void enqueue(heap *heap, node *new_node);
-
+/**
+ * @brief 
+ * 
+ * @param heap 
+ * @return node* 
+ */
 node *dequeue(heap *heap);
-
+/**
+ * @brief 
+ * 
+ * @param heap 
+ */
 void deallocate_heap(heap *heap);
 
 #endif
--- huffman/code/tree.h
+++ huffman/code/tree.h
@@ -2,7 +2,10 @@
 #define tree_h
 
 #include <stdio.h>
-
+/**
+ * @brief 
+ * 
+ */
 typedef struct node
 {
     void *item;
@@ -10,17 +13,52 @@ typedef struct node
     struct node *left;
     struct node *right;
 } node;
+/**
+ * @brief Create a node object
+ * 
+ * @return node* 
+ */
 
 node *create_node();
+/**
+ * @brief Create a tree object
+ * 
+ * @param item 
+ * @param left 
+ * @param right 
+ * @return node* 
+ */
 
 node *create_tree(void *item, node *left, node *right);
+/**
+ * @brief 
+ * 
+ * @param tree 
+ */
 
 void print_tree(node *tree);
-
+/**
+ * @brief 
+ * 
+ * @param tree 
+ * @param compacted_file 
+ */
 void save_tree(node *tree, FILE *compacted_file);
-
+/**
+ * @brief 
+ * 
+ * @return unsigned short* 
+ */
 unsigned short *allocate_counter();
-
+/**
+ * @brief 
+ * 
+ * @param tree 
+ */
 void deallocate_tree(node *tree);
+/**
+ * @brief 
+ * 
+ */
 
 #endif
