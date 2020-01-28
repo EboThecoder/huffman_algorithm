81336603ee185e239acac0fce3edde3da97d6a5e
root <root@DESKTOP-RLT2A7F.localdomain>
2020-01-27 21:35:35 -0300

    inicio dos unit tests

--- huffman/code/tests.c
+++ huffman/code/tests.c
@@ -1,9 +1,82 @@
-#include "CUnit/CUnit.h"
+#include <CUnit/CUnit.h>
+#include "tree.h"
 #include "heap.h"
 #include "hash.h"
-#include "tree.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void test_heap_creation()
+{
+    heap* heap = create_heap();
+    CU_ASSERT(heap->size == 0);
+    CU_ASSERT(heap->nodes);
+    for(i=0; i<256;i++)
+    {
+        CU_ASSERT_EQUAL('*', *(unsigned char *)new_node->item);
+        CU_ASSERT_EQUAL(new_node->frequency , 1);
+        CU_ASSERT_EQUAL(new_node->left , NULL);
+        CU_ASSERT_EQUAL(new_node->right , NULL);
+    }
+}
 
 int main()
 {
-    CU_psuit  psuit = NULL;
+    CU_pSuite pSuite = NULL;
+
+    if (CUE_SUCCESS != CU_initialize_registry())
+        return CU_get_error();
+
+    pSuite = CU_add_suite("Basic_Test_Suite1", init_suite, clean_suite);
+
+    if (NULL == pSuite)
+    {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    if ((NULL == CU_add_test(pSuite, "\n\n... Testing Heap Creation...\n\n", test_create_heap)))
+    {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    if ((NULL == CU_add_test(pSuite, "\n\n... Testing Heap Enqueue...\n\n", test_enqueue)))
+    {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    if ((NULL == CU_add_test(pSuite, "\n\n... Testing Heap Dequeue...\n\n", test_dequeue)))
+    {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    if ((NULL == CU_add_test(pSuite, "\n\n... Testing Tree Building...\n\n", test_tree_building)))
+    {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    if ((NULL == CU_add_test(pSuite, "\n\n... Testing New Hash Mapping...\n\n", test_new_hash_mapping)))
+    {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+    return CU_get_error();
+    return 0;
 }
